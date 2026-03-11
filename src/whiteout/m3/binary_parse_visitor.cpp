45c5431bb65b2518187ebc73cc47eed23f785436
Fernando Sahmkow <fsahmkow27@gmail.com>
2026-03-11 13:31:17 +0100

    Eliminate refactor comment

--- src/whiteout/m3/binary_parse_visitor.cpp
+++ src/whiteout/m3/binary_parse_visitor.cpp
@@ -1,18 +1,6 @@
 // SPDX-License-Identifier: BSD-3-Clause
 // Copyright (c) 2026 Fernando Sahmkow
 
-/// @file bc7.cpp
-/// @brief BC7 block-compression codec: RGBA8 ↔ BC7 decode and encode.
-///
-/// Fully decodes and encodes all 8 BC7 modes (0–7) according to the
-/// BC7/BPTC specification.  No external dependencies – pure C++20.
-///
-/// Implementation is split into .inl parts under bc7/ for readability:
-///   decode.inl         – mode table, block decoder, decode_texture
-///   encode_common.inl  – colour helpers, PCA, endpoint fitting
-///   encode_mode[0-7].inl – individual mode encoders
-///   encode_block.inl   – partition search, encode_block dispatch, encode_texture
-
 #include "../common/binary_reader.h"
 #include "binary_parse_visitor.h"
 #include "chunk_traits.h"
